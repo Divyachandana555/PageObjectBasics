08034925d220fb0dfa4df9243d498e46c2201ca0
HR <HR@INHYNITPOOL164.infor.com>
2020-03-22 16:04:30 +0530

    Committing EGit Changes

--- src/test/java/com/w2a/rough/LoginTest.java
+++ src/test/java/com/w2a/rough/LoginTest.java
@@ -17,7 +17,7 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 
-		
+		//Using EGit plugin
 		HomePage home = new HomePage();
 		LoginPage lp = home.goToLogin();
 		ZohoAppPage zp = lp.doLogin("divyapeddakotla@gmail.com", "Chandana1@");
